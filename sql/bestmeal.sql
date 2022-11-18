8adb7583051bc86db6c3d4c7578e23c49ae9db29
leticia <lefarias111@gmail.com>
2022-11-18 18:39:13 -0300

    banco

--- sql/bestmeal.sql
+++ sql/bestmeal.sql
@@ -1,14 +1,35 @@
-create database bestmeal;
-use bestmeal;
+--
+-- Banco de dados: `bestmeal`
+--
+CREATE DATABASE bestmeal;
+USE bestmeal;
 
-CREATE TABLE usuario (
-   id int NOT NULL AUTO_INCREMENT,
-   nome varchar(50) NOT NULL,
-   email varchar(255) NOT NULL,
-   senha varchar(60) NOT NULL,
-   ativo tinyint NOT NULL DEFAULT '0',
-   adm  tinyint NOT NULL DEFAULT '0',
-   PRIMARY KEY (id)  
-);
+-- --------------------------------------------------------
 
+--
+-- Estrutura da tabela `produtos`
+--
+
+CREATE TABLE `produtos` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `preco` varchar(255) NOT NULL,
+  `ingredientes` text NOT NULL,
+  `imagem` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `senha` varchar(60) NOT NULL,
+  `ativo` tinyint(4) NOT NULL DEFAULT 0,
+  `adm` tinyint(4) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
