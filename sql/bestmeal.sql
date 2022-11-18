9e1192f74a8255eb70558cec7687c88052cf450a
leticia <lefarias111@gmail.com>
2022-11-18 15:05:24 -0300

    banco

--- sql/bestmeal.sql
+++ sql/bestmeal.sql
@@ -1,3 +1,14 @@
-https://github.com/LetFa/TCC_Bestmeal
+create database bestmeal;
+use bestmeal;
+
+CREATE TABLE usuario (
+   id int NOT NULL AUTO_INCREMENT,
+   nome varchar(50) NOT NULL,
+   email varchar(255) NOT NULL,
+   senha varchar(60) NOT NULL,
+   ativo tinyint NOT NULL DEFAULT '0',
+   adm  tinyint NOT NULL DEFAULT '0',
+   PRIMARY KEY (id)  
+);
 
 
