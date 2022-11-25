d2176e6bf98b21fbc6cf0617995054bf8b84d569
leticia <lefarias111@gmail.com>
2022-11-25 18:07:28 -0300

    siteeeee

--- index.php
+++ index.php
@@ -9,8 +9,44 @@ require __DIR__ . "/sistema/bloqueio.php";
 <body>
   <?php require __DIR__ . '/views/home/componentes/navbar.php' ?>
 
-  <main>
-    <!--Carousel-->
+
+  <!--Carousel-->
+
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100" src="views/_resources/storage/banner/banner1.png" alt="First slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="views/_resources/storage/banner/banner2.png" alt="Second slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="views/_resources/storage/banner/banner3.png" alt="Third slide">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+
+
+
+
+
+
+
+  <!--Carousel-
     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
       <div class="carousel-inner ">
         <div class="carousel-item active">
@@ -31,190 +67,162 @@ require __DIR__ . "/sistema/bloqueio.php";
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
       </button>
-
-      <!--lanches-->
-      <div class="lanche-area">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-md-12">
-              <h1 class="text-center titulo-area">Conheça algumas criações</h1>
-            </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top " src="views/_resources/storage/lanches/lanche001.png" alt="x-american">
-                <div class="card-body">
-                  <p class="card-text text-dark text-center">X-AMERICAN</p>
-                
-                  <div class="lista-cardapio">
-                    <ul>
-                      <li><i class="bi bi-caret-right-fill"></i>2 Hambúrguer;</li>
-                      <li><i class="bi bi-caret-right-fill"></i>Alface;</li>
-                      <li><i class="bi bi-caret-right-fill"></i>Azeitona preta;</li>
-                      <li><i class="bi bi-caret-right-fill"></i>Cebola Roxa;</li>
-                      <li><i class="bi bi-caret-right-fill"></i>Pliques;</li>
-                      <li><i class="bi bi-caret-right-fill"></i>Bacon;</li>
-                    </ul>
-                  </div>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                    <button type="button" class="btn btn-warning">Comprar</button>
-                    <p class="card-text text-success font" style="margin-left:1rem;">R$ 15,99</p>
-                    </div>
-                    <small class="text-muted">Bestmeal</small>
-                  </div>
+-->
+  <!--lanches-->
+  <div class="lanche-area">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 class="text-center titulo-area">Conheça alguns lanches nossos</h1>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 box-shadow">
+            <img class="card-img-top " src="views/_resources/storage/lanches/lanche001.png" alt="x-american">
+            <div class="card-body">
+              <h5 class="text-center">X-AMERICAN</h5>
+              <p class="card-text text-dark text-center">Hambúrguer, cheddar, bacon, ketchup e pliques</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-warning">Comprar</button>
+                  <p class="card-text text-success font" style="margin-left:1rem;">R$ 15,99</p>
                 </div>
+                <small class="text-muted">Bestmeal</small>
               </div>
             </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top " src="views/_resources/storage/lanches/lanche002.png" alt="x-american">
-                <div class="card-body">
-                  <p class="card-text text-center">X-TORRE EIFFEL</p>
-                  <div class="lista-cardapio">
-                    <ul>
-                      <li><i class="bi bi-caret-right-fill"></i>3 Hambúrguer;</li>
-                      <li><i class="bi bi-caret-right-fill"></i>Alface;</li>
-                      <li><i class="bi bi-caret-right-fill"></i>Cheddar;</li>
-                      <li><i class="bi bi-caret-right-fill"></i>Cebola;</li>
-                      <li><i class="bi bi-caret-right-fill"></i>Pliques;</li>
-                      <li><i class="bi bi-caret-right-fill"></i>Tomate;</li>
-                    </ul>
-                  </div>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-warning">Comprar</button>
-                      <p class="card-text text-success font" style="margin-left:1rem;">R$ 13,99</p>
-                    </div>
-                    <small class="text-muted">Bestmeal</small>
-                  </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 box-shadow">
+            <img class="card-img-top " src="views/_resources/storage/lanches/lanche002.png" alt="x-american">
+            <div class="card-body">
+              <h5 class="text-center">X-TORRE EIFFEL</h5>
+              <p class="card-text text-dark text-center">Hambúrguer, cheddar, bacon, ketchup e pliques</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-warning">Comprar</button>
+                  <p class="card-text text-success font" style="margin-left:1rem;">R$ 13,99</p>
                 </div>
+                <small class="text-muted">Bestmeal</small>
               </div>
             </div>
-            <div class="col-md-4">
-              <div class="card mb-4 box-shadow">
-                <img class="card-img-top " src="views/_resources/storage/lanches/lanche003.png" alt="x-american">
-                <div class="card-body">
-                  <p class="card-text text-center">X-BACON</p>
-                  <div class="lista-cardapio">
-                    <ul>
-                      <li><i class="bi bi-caret-right-fill"></i>1 Hambúrguer;</li>
-                      <li><i class="bi bi-caret-right-fill"></i>Alface;</li>
-                      <li><i class="bi bi-caret-right-fill"></i>Queijo;</li>
-                      <li><i class="bi bi-caret-right-fill"></i>Cebola;</li>
-                      <li><i class="bi bi-caret-right-fill"></i>Bacon;</li>
-                      <li><i class="bi bi-caret-right-fill"></i>Tomate;</li>
-                    </ul>
-                  </div>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-warning">Comprar</button>
-                      <p class="card-text text-success font" style="margin-left:1rem;">R$ 17,99</p>
-                    </div>
-                    <small class="text-muted">Bestmeal</small>
-                  </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 box-shadow">
+            <img class="card-img-top " src="views/_resources/storage/lanches/lanche003.png" alt="x-american">
+            <div class="card-body">
+              <h5 class="text-center">X-BACON</h5>
+              <p class="card-text text-dark text-center">Hambúrguer, cheddar, bacon, ketchup e pliques</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-warning">Comprar</button>
+                  <p class="card-text text-success font" style="margin-left:1rem;">R$ 17,99</p>
                 </div>
+                <small class="text-muted">Bestmeal</small>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+  </div>
+  </div>
 
-    <!--Equipe-->
-    <div class="team-area">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <h2 class="text-center titulo-area team-titulo">Nossa equipe</h2>
-          </div>
-          <div class="col-md-3">
-            <div class="card time mt-5">
-              <img src="views/_resources/storage/time/equipe1.jpeg" class="card-img-top" alt="imagem de perfil 1">
-              <div class="card-body">
-                <h5 class="card-title">Letícia Farias</h5>
-                <p class="card-text">Estudante do IF</p>
-              </div>
+  <!--Equipe-->
+  <div class="team-area">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h2 class="text-center titulo-area team-titulo">Nossa equipe</h2>
+        </div>
+        <div class="col-md-3">
+          <div class="card time mt-5">
+            <img src="views/_resources/storage/time/equipe1.jpeg" class="card-img-top" alt="imagem de perfil 1">
+            <div class="card-body">
+              <h5 class="card-title">Letícia Farias</h5>
+              <p class="card-text">Estudante do IF</p>
             </div>
           </div>
-          <div class="col-md-3">
-            <div class="card time mt-5">
-              <img src="views/_resources/storage/time/equipe2.jpeg" class="card-img-top" alt="imagem de perfil 2">
-              <div class="card-body">
-                <h5 class="card-title">Larissa Farias</h5>
-                <p class="card-text">Estudante do IF</p>
-              </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card time mt-5">
+            <img src="views/_resources/storage/time/equipe2.jpeg" class="card-img-top" alt="imagem de perfil 2">
+            <div class="card-body">
+              <h5 class="card-title">Larissa Farias</h5>
+              <p class="card-text">Estudante do IF</p>
             </div>
           </div>
-          <div class="col-md-3">
-            <div class="card time mt-5">
-              <img src="views/_resources/storage/time/equipe3.jpeg" class="card-img-top" alt="imagem de perfil 3">
-              <div class="card-body">
-                <h5 class="card-title">João Vitor</h5>
-                <p class="card-text">Estudante do IF</p>
-              </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card time mt-5">
+            <img src="views/_resources/storage/time/equipe3.jpeg" class="card-img-top" alt="imagem de perfil 3">
+            <div class="card-body">
+              <h5 class="card-title">João Vitor</h5>
+              <p class="card-text">Estudante do IF</p>
             </div>
           </div>
-          <div class="col-md-3 mt-5">
-            <div class="card time time-ste">
-              <img src="views/_resources/storage/time/equipe4.jpeg" class="card-img-top" alt="imagem de perfil 4">
-              <div class="card-body">
-                <h5 class="card-title">Stephanie Amorim</h5>
-                <p class="card-text">Estudante do IF</p>
-              </div>
+        </div>
+        <div class="col-md-3 mt-5">
+          <div class="card time time-ste">
+            <img src="views/_resources/storage/time/equipe4.jpeg" class="card-img-top" alt="imagem de perfil 4">
+            <div class="card-body">
+              <h5 class="card-title">Stephanie Amorim</h5>
+              <p class="card-text">Estudante do IF</p>
             </div>
           </div>
         </div>
       </div>
     </div>
+  </div>
 
-    <!--Quem somos-->
-    <div id="apply-area">
-      <div class="container-fluid mt-5">
-        <div class="row">
-          <div class="col-md-6 apply-box" id="company-img"></div>
-          <div class="col-md-6 apply-box" id="pattern-img">
-            <h4 class="text-center">Quem somos?</h4>
-            <p class="text-center">Você deve estar se perguntando: Quem desenvolve uma plataforma, onde o cliente é o próprio inventor de
-              sua alimentação? </p>
-            <p class="text-center"> E por aqui eu digo, somos nós, o Bestmeal! Desenvolvido em 2022, por uma equipe especializada, este
-              projeto tende-se à apresentar uma variedade de escolhas, onde você cliente, é o próprio chefe de seu
-              lanche ou bebida, apresentando seus maiores gostos por culinária, dentro de seus padrões alimentícios.
-            </p>
-            <p class="text-center">
-              Quem não queria um site de lanches onde você pode inventar o que quiser? Um lanche vegano? Você pode ter!
-              Um lanche sem glúten? Aqui tem! Um lanche com tudo que você possa gostar? É aqui o seu lugar!</p>
-            <p class="text-center">Clique no botão abaixo e conheça um pouquinho sobre nós!</p>
-            <div class="text-center">
+  <!--Quem somos-->
+  <div id="apply-area">
+    <div class="container-fluid mt-5">
+      <div class="row">
+        <div class="col-md-6 apply-box" id="company-img"></div>
+        <div class="col-md-6 apply-box" id="pattern-img">
+          <h4 class="text-center">Quem somos?</h4>
+          <p class="text-center">Você deve estar se perguntando: Quem desenvolve uma plataforma, onde o cliente é o próprio inventor de
+            sua alimentação? </p>
+          <p class="text-center"> E por aqui eu digo, somos nós, o Bestmeal! Desenvolvido em 2022, por uma equipe especializada, este
+            projeto tende-se à apresentar uma variedade de escolhas, onde você cliente, é o próprio chefe de seu
+            lanche ou bebida, apresentando seus maiores gostos por culinária, dentro de seus padrões alimentícios.
+          </p>
+          <p class="text-center">
+            Quem não queria um site de lanches onde você pode inventar o que quiser? Um lanche vegano? Você pode ter!
+            Um lanche sem glúten? Aqui tem! Um lanche com tudo que você possa gostar? É aqui o seu lugar!</p>
+          <p class="text-center">Clique no botão abaixo e conheça um pouquinho sobre nós!</p>
+          <div class="text-center">
             <a href="#" class="main-btn" id="apply-btn">Saíba Mais</a>
-            </div>
           </div>
         </div>
       </div>
     </div>
+  </div>
 
-    <!--Circle area-->
-    <div id="data-area">
-      <div class="container mt-5">
-        <div class="row">
-          <div class="col-md-3 circle-box">
-            <div id="circleA"></div>
-            <p>Lanches Entregues</p>
-          </div>
-          <div class="col-md-3 circle-box">
-            <div id="circleB"></div>
-            <p>Clientes Felizes</p>
-          </div>
-          <div class="col-md-3 circle-box">
-            <div id="circleC"></div>
-            <p>Colaboradores</p>
-          </div>
-          <div class="col-md-3 circle-box">
-            <div id="circleD"></div>
-            <p>Hamburguinhos</p>
-          </div>
+  <!--Circle area-->
+  <div id="data-area">
+    <div class="container mt-5">
+      <div class="row">
+        <div class="col-md-3 circle-box">
+          <div id="circleA"></div>
+          <p>Lanches Entregues</p>
+        </div>
+        <div class="col-md-3 circle-box">
+          <div id="circleB"></div>
+          <p>Clientes Felizes</p>
+        </div>
+        <div class="col-md-3 circle-box">
+          <div id="circleC"></div>
+          <p>Colaboradores</p>
+        </div>
+        <div class="col-md-3 circle-box">
+          <div id="circleD"></div>
+          <p>Hamburguinhos</p>
         </div>
       </div>
     </div>
+  </div>
   </main>
 
   <?php if (isset($_GET['criado'])) { ?>
@@ -242,6 +250,7 @@ require __DIR__ . "/sistema/bloqueio.php";
   <?php require __DIR__ . "/views/home/componentes/footer.php" ?>
   <script src="./views/_resources/separate/progressBar.min.js" defer></script>
   <script src="./views/_resources/separate/circle.js" defer></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 </body>
 
--- views/home/componentes/head.php
+++ views/home/componentes/head.php
@@ -5,9 +5,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
-  integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
 
   <!---Links CSS-->
   <link rel="stylesheet" href="./views/_resources/css/_theme.css">
@@ -23,5 +22,10 @@
   <!--icon bootstrap-->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 
-  <script src="./views/_resources/js/bootstrap.min.js"></script>
+   
+  
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  
 </head>
